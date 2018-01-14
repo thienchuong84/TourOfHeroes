115e59faa341dc2a9b8d463e2791004ecd4b9dfe
thienchuong1411nn2 <35033951+thienchuong1411nn2@users.noreply.github.com>
2018-01-14 17:11:58 +0700

    first commit, complete section 3 of tutorial angular2. Show hero id, use pipe, show input to type and two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import FormsModule to use ngModel in hero component.html
+// after declare here, declare it in imports
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
