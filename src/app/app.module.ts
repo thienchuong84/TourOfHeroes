02cbe4606607d6cf3e365fa96d8cac4972f44cb0
thienchuong1411nn2 <35033951+thienchuong1411nn2@users.noreply.github.com>
2018-01-14 22:49:14 +0700

    completed section 5: Master/Detail component. move detail hero from hero component to hero detail component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
