e695542d34d4cc1eb14c13a675cc698089469e9f
Chuong Nguyen <thienchuong1411nn2@gmail.com>
2018-01-16 14:17:13 +0700

    Section 8 HTTP, completed to rewrite getHeroes and getHero by Id use angular-in-memory-web-api and replace mock-data by get data from in-memory-data.service to save delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { MessageService } from './message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 @NgModule({
   declarations: [
@@ -27,7 +31,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses
+    // remove it when a real server is ready to receive requests
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [ HeroService, MessageService],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,27 +8,76 @@ import { HEROES } from './mock-heroes';
 
 import { MessageService } from './message.service';
 
+import { catchError, tap } from 'rxjs/operators';
+
+// section 8
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
   // muốn sử dụng method, functions trong server thì khởi tạo nó trong constructure
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
 
   // Add a getHeroes method to return the mock heroes.
   // getHeroes: Hero[] {
   //   return HEROES;
   // }
 
-  getHeroes(): Observable<Hero[]> {
-    // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+
+  getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    // this.messageService.add('HeroService: fetched heroes');
+
+    // return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+  // getHero(id: number): Observable<Hero> {
+  //   // todo: send message _after_ fetching the hero 
+  //   this.messageService.add(`HeroService: fetched hero id=${id}`);
+  //   return of(HEROES.find(hero => hero.id === id));
+  // }
+
   getHero(id: number): Observable<Hero> {
-    // todo: send message _after_ fetching the hero 
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
 }
